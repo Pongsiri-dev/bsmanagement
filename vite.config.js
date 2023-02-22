61b9d9dd15e011644a046cabedb2e7c174938ed3
Pongsiri-dev <Taa_wappey@windowslive.com>
2023-02-23 01:00:17 +0700

    Update vite config x2

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'resources/js/feedback.js',
-                    dest: 'js/custom.js'
-                }
-            ]
-        })
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
 });
