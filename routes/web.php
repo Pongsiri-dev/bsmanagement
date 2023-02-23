235402baca45b813c3354c676680afcce3a0c674
Pongsiri-dev <Taa_wappey@windowslive.com>
2023-02-23 18:14:08 +0700

    update config cache

--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,29 @@ Route::post('/CheckInEvents', [EventController::class, 'checkIn'])->name('CheckI
 
 // QR CODE
 Route::get('/generate-qrcode', [QrCodeController::class, 'index']);
+
+
+// Cache
+//Clear route cache:
+Route::get('/route-cache', function () {
+    $exitCode = Artisan::call('route:cache');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache:
+Route::get('/config-cache', function () {
+    $exitCode = Artisan::call('config:cache');
+    return 'Config cache cleared';
+});
+
+// Clear application cache:
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cache:
+Route::get('/view-clear', function () {
+    $exitCode = Artisan::call('view:clear');
+    return 'View cache cleared';
+});
