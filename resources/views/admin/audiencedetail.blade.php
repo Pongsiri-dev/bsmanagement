4f3808bf0b40f9cbe44a9fc9e93085fa3d99bed0
Pongsiri-dev <Taa_wappey@windowslive.com>
2023-02-25 01:31:04 +0700

    update table audience

--- resources/views/admin/audiencedetail.blade.php
+++ resources/views/admin/audiencedetail.blade.php
@@ -100,7 +100,7 @@
                                             {{ SlashDateOnly($item->created_at) }}
                                         </p>
                                     </td>
-                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-center text-sm">
                                         <span
                                             class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                                             <span aria-hidden
